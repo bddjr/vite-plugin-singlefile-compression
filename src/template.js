dd069b926102399df5e0c2a9a755dce84f4ad4a5
bddjr <bddjr08@outlook.com>
2024-12-29 23:26:25 +0800

    1.1.2 optimize template.js

--- src/template.js
+++ src/template.js
@@ -1,9 +1,8 @@
 //@ts-nocheck
 
 fetch("data:application/gzip;base64,{<script>}")
-	.then(r => r.blob())
-	.then(b => new Response(
-		b.stream().pipeThrough(new DecompressionStream("gzip")),
+	.then(r => new Response(
+		r.body.pipeThrough(new DecompressionStream("gzip")),
 		{ headers: { "Content-Type": "text/javascript" } }
 	).blob())
 	.then(b =>
