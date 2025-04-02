dcac79594169e0426f270524a5281e4aafda82e0
bddjr <bddjr08@outlook.com>
2025-04-02 23:35:47 +0800

    逼养的typescript😡

--- src/index.ts
+++ src/index.ts
@@ -169,7 +169,7 @@ async function generateBundle(bundle: OutputBundle, config: ResolvedConfig, opti
                     thisDel.add(bundleName)
                     oldSize += a.source.length
                     if (!Object.hasOwn(globalAssetsDataURL, name))
-                        globalAssetsDataURL[name] = bufferToDataURL(name, Buffer.from(a.source))
+                        globalAssetsDataURL[name] = bufferToDataURL(name, Buffer.from(a.source as Uint8Array<ArrayBufferLike>))
                     assetsDataURL[name] = globalAssetsDataURL[name]
                 }
                 element.src = `data:${name}`
