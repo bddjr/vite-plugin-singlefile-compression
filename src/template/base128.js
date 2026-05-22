117a6c004076702def04738d1419d513f439027b
bddjr <bddjr08@outlook.com>
2026-05-22 15:45:53 +0800

    optimize template base128

--- src/template/base128.js
+++ src/template/base128.js
@@ -2,31 +2,27 @@
 
 // License: Unlicense
 
-for (var /**@type {string}*/ input = __SPLIT__
+for (var R = Response
+    , D = document
+    , script = D.createElement("script")
+    , input = __SPLIT__
     , il = input.length
     , out = new Uint8Array(il / 8 * 7)
     , ii = 0
     , oi = 0
+    , k
     , cache
-    , next = _ => (
-        (cache = input.charCodeAt(ii++)) > 127
+    , next = _ =>
+        (cache = input.charCodeAt(ii++)) >> 7
             ? cache = 0 // In HTML, 0 is likely to be converted to 65533 (�)
             : cache
-    )
-    , R = Response
-    , script = document.createElement("script")
     ; ii < il;) {
     //     0        1        2        3        4        5        6        7
     // in  _0000000 _1111111 _2222222 _3333333 _4444444 _5555555 _6666666 _7777777
     // out 00000001 11111122 22222333 33334444 44455555 55666666 67777777
-
-    /* 0 */ out[oi++] = next() << 1 | next() >> 6
-    /* 1 */ out[oi++] = cache << 2 | next() >> 5
-    /* 2 */ out[oi++] = cache << 3 | next() >> 4
-    /* 3 */ out[oi++] = cache << 4 | next() >> 3
-    /* 4 */ out[oi++] = cache << 5 | next() >> 2
-    /* 5 */ out[oi++] = cache << 6 | next() >> 1
-    /* 6 */ out[oi++] = cache << 7 | next()
+    for (next(), k = 7; k;) {
+        out[oi++] = cache << 7 - --k | next() >> k
+    }
 }
 
 new R(
@@ -34,5 +30,5 @@ new R(
 ).text().then(t => {
     script.type = 'module'
     script.innerHTML = t
-    document.head.appendChild(script)
+    D.head.appendChild(script)
 });
