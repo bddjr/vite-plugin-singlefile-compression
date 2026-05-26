04ece186d893ed8793967c2a0b887a08c896e7e7
bddjr <bddjr08@outlook.com>
2026-05-26 23:15:33 +0800

    optimize template base128

--- src/template/base128.js
+++ src/template/base128.js
@@ -16,18 +16,14 @@
             (cache = input.charCodeAt(ii++)) >> 7
                 ? cache = 0 // In HTML, 0 is likely to be converted to 65533 (�)
                 : cache
-    for (; ii < __SPLIT__; out[oi++] = cache << 8 - k | next() >> --k) {
-        //     0        1        2        3        4        5        6        7
-        // in  _0000000 _1111111 _2222222 _3333333 _4444444 _5555555 _6666666 _7777777
-        // out 00000001 11111122 22222333 33334444 44455555 55666666 67777777
-        k || next(k = 7)
-    }
+
+    for (script.type = 'module'; ii < __SPLIT__; out[oi++] = cache << 8 - k | next() >> --k)
+        k || next(k = 7);
 
     new R(
         new R(out).body.pipeThrough(new DecompressionStream(__SPLIT__))
     ).text().then(t => {
-        script.type = 'module'
         script.innerHTML = t
         D.head.appendChild(script)
-    });
+    })
 }
